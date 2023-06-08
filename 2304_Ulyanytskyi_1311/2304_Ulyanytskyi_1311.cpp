976fecd30911003a28c3fcbfb86b31dd46e45323
Remas Orca <remas.orca@gmail.com>
2023-06-08 21:26:55 +0300

    v1

--- 2304_Ulyanytskyi_1311/2304_Ulyanytskyi_1311.cpp
+++ 2304_Ulyanytskyi_1311/2304_Ulyanytskyi_1311.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
+#include <cstdint>
 
 using namespace std;
 
+int get_user_value();
+int get_factirial(int number);
+
 int main()
 {
-    int students_amont;
+    int students_number, seats_number;
+    int result;
+
+    cout << "Enter the number of students: ";
+    students_number = get_user_value();
+
+    cout << "Enter the number of seats: ";
+    seats_number = get_user_value();
+    
+    if (students_number > seats_number) {
+        cout << "More students than places!";
+    }
+    else
+    {
+        result = get_factirial(seats_number) / get_factirial(seats_number - students_number);
+        cout << result << " options to place " << students_number;
+        cout << " students in " << seats_number << " seats";
+    }
 
     return 0;
+}
+
+int get_user_value() {
+    int value;
+    cin >> value;
+    return value;
+}
+
+int get_factirial(int number) {
+    if (number == 0 || number == 1) {
+        return 1;
+    }
+    else {
+        return number * get_factirial(number - 1);
+    }
 }
